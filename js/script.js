cc2de7fcb16c09e204a58647b2c6c09ae03c38b5
spivak-m <spivak.makson@gmail.com>
2017-02-21 19:02:28 +0500

    beta 2.0

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ var closeContact = document.querySelector(".write-us-popup .close-btn");
 var closeMap = document.querySelector(".map-popup .close-btn");
 var popupContact = document.querySelector(".write-us-popup");
 var popupMap = document.querySelector(".map-popup");
+var contactForm = popupContact.querySelector("form");
+var loginInput = contactForm.querySelector("[name=your-name]");
+var emailInput = contactForm.querySelector("[name=your-email]");
+var questionInput = contactForm.querySelector("[name=your-text]");
 
 linkContact.addEventListener("click", function(event) {
 	event.preventDefault();
 	popupContact.classList.add("write-us-popup-show");
+	loginInput.focus();
 });
 
 closeContact.addEventListener("click", function(event) {
@@ -23,4 +28,37 @@ linkMap.addEventListener("click", function(event) {
 closeMap.addEventListener("click", function(event) {
 	event.preventDefault();
 	popupMap.classList.remove("map-popup-show");
+});
+
+contactForm.addEventListener("submit", function(event) {
+	if (!loginInput.value || !emailInput.value || !questionInput.value) {
+		event.preventDefault();
+	}
+});
+
+contactForm.addEventListener("submit", function(event) {
+	if (!loginInput.value) {
+		event.preventDefault();
+		loginInput.classList.add("input-novalue");
+	} else {
+		loginInput.classList.remove("input-novalue");
+	}
+});
+
+contactForm.addEventListener("submit", function(event) {
+	if (!emailInput.value) {
+		event.preventDefault();
+		emailInput.classList.add("input-novalue");
+	} else {
+		emailInput.classList.remove("input-novalue");
+	}
+});
+
+contactForm.addEventListener("submit", function(event) {
+	if (!questionInput.value) {
+		event.preventDefault();
+		questionInput.classList.add("input-novalue");
+	} else {
+		questionInput.classList.remove("input-novalue");
+	}
 });
